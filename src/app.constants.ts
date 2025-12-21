6f00fdd22e8005d96d3df4006048dd8de4890471
Ivan <ipkozyrin@gmail.com>
2025-12-21 13:00:45 -0300

    fix

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -6,4 +6,4 @@
  * Graceful shutdown timeout in milliseconds
  * Maximum time to wait for in-flight requests to complete before forcing shutdown
  */
-export const GRACEFUL_SHUTDOWN_TIMEOUT_MS = 30000;
+export const GRACEFUL_SHUTDOWN_TIMEOUT_MS = 25000;
